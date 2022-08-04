6bdda69d04b7d1a858f67af720fc5f4b8a2c31a0
unknown <willhc1984@gmail.com>
2022-08-04 00:35:41 -0300

    Parking - Get/GetById/Post

--- src/main/java/com/dio/cloudparking/service/ParkingService.java
+++ src/main/java/com/dio/cloudparking/service/ParkingService.java
@@ -1,11 +1,10 @@
 package com.dio.cloudparking.service;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -19,8 +18,11 @@ public class ParkingService {
 	
 	static {
 		var id = getUUID();
+		var id1 = getUUID();
 		Parking parking = new Parking(id, "EWV-0125", "SP", "VW", "Blue", null, null, null);
+		Parking parking1 = new Parking(id1, "EWV-2345", "MG", "Ford", "Green", null, null, null);
 		parkingMap.put(id, parking);
+		parkingMap.put(id1, parking1);
 	}
 	
 	public List<Parking> findAll(){
@@ -29,6 +31,18 @@ public class ParkingService {
 
 	private static String getUUID() {		
 		return UUID.randomUUID().toString().replace("-", "");
+	}
+
+	public Parking findById(String id) {		
+		return parkingMap.get(id);
+	}
+	
+	public Parking create(Parking parkingCreate) {
+		String uuid = getUUID();
+		parkingCreate.setId(getUUID());
+		parkingCreate.setEntryDate(LocalDateTime.now());
+		parkingMap.put(uuid, parkingCreate);
+		return parkingCreate;
 	}
 
 }
